d063f4645ba8a84ed83ba5791ecc13e7e92a9688
Syahrul Apriansyah <apriansyah.syahrul@gmail.com>
2024-04-15 16:20:22 +0700

    -

--- static/js/script.js
+++ static/js/script.js
@@ -28,33 +28,39 @@ $(document).ready(function() {
         });
     });
 
-    function displayUserInfo(data) {
-        let content = `<div class="card mb-3" style="max-width: 540px;">
-                           <div class="row g-0">
-                               <div class="col-md-4">
-                                   <img src="${data.image}" class="img-fluid rounded-start" alt="${data.name}">
-                               </div>
-                               <div class="col-md-8">
-                                   <div class="card-body">
-                                       <h5 class="card-title">${data.name}</h5>`;
+function displayUserInfo(data) {
+    let jsonResponse = `<div class="mb-3">
+                            <h4 class="text-muted">API Response:</h4>
+                            <pre>${JSON.stringify(data, null, 2)}</pre>
+                        </div>`;
 
-        if (data.age) {
-            content += `<p class="card-text"><small class="text-muted">Age: ${data.age}</small></p>`;
-        }
-        if (data.gender) {
-            content += `<p class="card-text"><small class="text-muted">Gender: ${data.gender}</small></p>`;
-        }
-        if (data.hobby) {
-            content += `<p class="card-text">Hobby: ${data.hobby}</p>`;
-        }
-        if (data.quote) {
-            content += `<p class="card-text"><i>"${data.quote}"</i></p>`;
-        }
-        content += `           </div>
-                               </div>
-                           </div>
-                       </div>`;
+    let card = `<div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                            <img src="${data.image}" class="img-fluid rounded-start" alt="${data.name}">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">${data.name}</h5>`;
 
-        $('#result').html(content);
+    if (data.age) {
+        card += `<p class="card-text"><small class="text-muted">Age: ${data.age}</small></p>`;
     }
+    if (data.gender) {
+        card += `<p class="card-text"><small class="text-muted">Gender: ${data.gender}</small></p>`;
+    }
+    if (data.hobby) {
+        card += `<p class="card-text">Hobby: ${data.hobby}</p>`;
+    }
+    if (data.quote) {
+        card += `<p class="card-text"><i>"${data.quote}"</i></p>`;
+    }
+    card += `           </div>
+                        </div>
+                    </div>
+                </div>`;
+
+    $('#result').html(jsonResponse + card);
+}
+
 });
